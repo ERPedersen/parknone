e3422f26c251c38141a279a532690db3d116a644
Emil Rosenius Pedersen <erp@emilrosenius.com>
2020-11-14 17:13:54 +0100

    temp

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const { Gpio } = require( 'onoff' );
+const { Gpio } = require('onoff');
 
 // Set GPIO X as output
-const connection = new Gpio( 23, 'out' );
+const connection = new Gpio(23, 'out');
 
-// Send high
 connection.writeSync(Gpio.HIGH);
-connection.writeSync(Gpio.LOW);
 
-// Wait 1000 ms
-// setTimeout(function () {
-//   Send low
-// }, 1);
+setTimeout(function () {
+  connection.writeSync(Gpio.LOW);
+}, 5000);
